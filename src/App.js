367c4051a7c5669e478578061d7838c0ff0da70e
mustafa <mltm0204@gmail.com>
2021-12-04 17:38:56 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Cards from './components/cards/Cards';
 import Header from './components/header/Header';
 import Navbar from "./components/navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Header/>
+      <Cards/>
     </div>
   );
 }
